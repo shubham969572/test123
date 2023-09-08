f277838d0d046a33736c445fec9245952a861212
shubham969572 <suneel.verma@kiet.edu>
2023-09-08 11:56:22 +0530

    this is 1 commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+
+export default function Home() {
+  const Innercompo=()=>{
+    return(
+      <h1> hellodfgdrftgdftgdf sunil </h1>
+    )
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+      <h1> hello next js am leraning here jjjj</h1>
+      <DataName name="verma"/>
+      <Innercompo/>
+    </main>
+  )
+}
+
+const DataName=(props)=>{
+  return(
+    <h1> hello sunil {props.name} </h1>
+   
+   
+  )
+}
