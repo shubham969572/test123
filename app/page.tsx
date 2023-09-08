3fb2aad4c0d0f09f425dbff3fbfc501f679861de
shubham969572 <suneel.verma@kiet.edu>
2023-09-08 16:15:34 +0530

    fsdfeewqrfew

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     
-      <h1> hello next js am leraning here jjjj</h1>
+      <h1> hello next js am leraning here jjjjdgdsreftgdsretgsretg</h1>
       <DataName name="verma"/>
       <Innercompo/>
     </main>
